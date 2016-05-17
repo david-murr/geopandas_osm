5e7a4461bfe2cc8689566df509d7861873f50fcf
david-murr <david-murr@users.noreply.github.com>
2016-05-17 07:47:10 -0400

    Update osm.py
    handle empty nodes

--- geopandas_osm/osm.py
+++ geopandas_osm/osm.py
@@ -273,10 +273,12 @@ def read_relations(doc):
 def render_to_gdf(osmdata, drop_untagged=True):
     nodes = render_nodes(osmdata.nodes, drop_untagged)
 
-    ways = render_ways(osmdata.nodes, osmdata.waynodes, osmdata.waytags)
-    if ways is not None:
-        # We should get append working
-        nodes = nodes.append(ways).set_geometry('geometry', crs=_crs)
+    if nodes is None:
+        nodes = ways
+    else:
+        if ways is not None:
+            # We should get append working
+            nodes = nodes.append(ways).set_geometry('geometry', crs=_crs)
 
     return nodes
 
