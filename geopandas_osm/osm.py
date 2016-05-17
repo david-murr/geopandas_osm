1a2af2084cdcfa56dcb0b20faba1e35ae68a536d
david-murr <david-murr@users.noreply.github.com>
2016-05-17 08:15:03 -0400

    Update osm.py
    debug

--- geopandas_osm/osm.py
+++ geopandas_osm/osm.py
@@ -272,6 +272,7 @@ def read_relations(doc):
 
 def render_to_gdf(osmdata, drop_untagged=True):
     nodes = render_nodes(osmdata.nodes, drop_untagged)
+    ways = render_ways(osmdata.nodes, osmdata.waynodes, osmdata.waytags)
 
     if nodes is None:
         nodes = ways
